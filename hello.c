3127f638a5ff1496dee4475b79199a16d5a738f7
Jason Pepas <jasonpepas@gmail.com>
2015-11-19 21:32:30 -0600

    adding source files

---
+++ hello.c
@@ -0,0 +1,16 @@
+/* hello.c
+ * See https://github.com/pepaslabs/hello.c
+ * Copyright (C) 2015 Jason Pepas.
+ * Released under the terms of the MIT license.
+ * See https://opensource.org/licenses/MIT
+ */
+
+#include "hello.h"
+
+#include <stdio.h>
+
+
+void hello()
+{
+	printf("Hello, world!\n");
+}
---
+++ hello.h
@@ -0,0 +1,10 @@
+/* hello.h
+ * See https://github.com/pepaslabs/hello.c
+ * Copyright (C) 2015 Jason Pepas.
+ * Released under the terms of the MIT license.
+ * See https://opensource.org/licenses/MIT
+ */
+
+
+// prints the string "Hello, world!\n" to stdout.
+void hello();
